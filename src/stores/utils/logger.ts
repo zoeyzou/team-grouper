32069b32f811527bde0e08dc7ebd8706219a5454
Zoey Zou <13701719+zoeyzou@users.noreply.github.com>
2020-08-01 10:17:13 +0200

    Initial commit

---
+++ src/stores/utils/logger.ts
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+const logger = (store: any) => (next: any) => (action: any) => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  const result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd()
+  return result
+}
+/* eslint-enable no-console */
+
+export default logger
